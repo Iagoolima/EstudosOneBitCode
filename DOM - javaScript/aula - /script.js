649de1e0ff76356ec1f8e9bee6782720780064d5
iagoolima <iagosilvalima22@outlook.com>
2022-11-02 20:15:19 -0300

    aulas

---
+++ DOM - javaScript/aula - /script.js
@@ -0,0 +1,16 @@
+//criar elementos
+
+function addInput(){
+    const ul = document.getElementById('inputs')
+
+    const newLi = document.createElement('li')
+    newLi.className = 'list-item'
+    newLi.innerText = 'novo input'
+
+    const newInput = document.createElement('input')
+    newInput.type = 'Text'
+    newInput.name = 'input'
+
+    newLi.appendChild(newInput)
+    ul.appendChild(newLi)
+}
